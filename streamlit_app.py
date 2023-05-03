ce867a4ea970a1835c5a47afe3f2f572ffed854e
nirmal2408 <132449155+nirmal2408@users.noreply.github.com>
2023-05-03 16:42:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.header('Breakfast Menu')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+import pandas
